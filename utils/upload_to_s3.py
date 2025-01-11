5f9df4a64e340dc1cdde5603c9c308be70229ae5
Kunal Sharma <mailkunalsharma99@gmail.com>
2025-01-11 18:14:02 +0530

    update file sanitisation

--- utils/upload_to_s3.py
+++ utils/upload_to_s3.py
@@ -34,12 +34,24 @@ def get_s3_presigned_url(bucket_name: str, object_name: str) -> Optional[str]:
 
 def sanitize_object_name(object_name: str) -> str:
     """
-    Sanitize the object name to allow only safe characters.
+    Sanitize the object name to allow only safe characters and limit length to 50 chars.
 
     :param object_name: Original object name
     :return: Sanitized object name
     """
-    return re.sub(r"[^a-zA-Z0-9._-]", "_", object_name)
+    # First remove unsafe characters
+    sanitized = re.sub(r"[^a-zA-Z0-9._-]", "", object_name)
+    
+    # If filename is longer than 50 chars, truncate it
+    # We preserve the file extension by splitting and handling separately
+    if len(sanitized) > 50:
+        name, ext = os.path.splitext(sanitized)
+        # Take first (50 - length of extension) characters of the name
+        # and append the extension
+        max_name_length = 50 - len(ext)
+        sanitized = name[:max_name_length] + ext
+    
+    return sanitized
 
 
 def upload_to_s3(file_path: str, object_name: str) -> Optional[str]:
