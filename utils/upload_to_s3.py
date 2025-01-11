5e428038fd56a7bbac03d3621f52f0890fc718a2
Kunal Sharma <mailkunalsharma99@gmail.com>
2025-01-11 19:09:15 +0530

    updated

--- utils/upload_to_s3.py
+++ utils/upload_to_s3.py
@@ -41,20 +41,17 @@ def sanitize_object_name(object_name: str) -> str:
     """
     # First remove unsafe characters
     sanitized = re.sub(r"[^a-zA-Z0-9._-]", "", object_name)
-    
-    #handle cases where filename comes as empty
-    if len(sanitized) == 0:
-        sanitized = "video-file"
+
     # If filename is longer than 50 chars, truncate it
     # We preserve the file extension by splitting and handling separately
-    elif len(sanitized) > 50:
+    if len(sanitized) > 50:
         name, ext = os.path.splitext(sanitized)
         # Take first (50 - length of extension) characters of the name
         # and append the extension
         max_name_length = 50 - len(ext)
         sanitized = name[:max_name_length] + ext
     
-    return sanitized
+    return "vidf"+ sanitized
 
 
 def upload_to_s3(file_path: str, object_name: str) -> Optional[str]:
